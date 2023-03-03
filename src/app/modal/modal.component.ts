1e7e27f94b725b848335e76822019b4a9c4b2239
Sothon Sitsiwalai <sothon@odds.team>
2023-03-03 16:52:49 +0700

    add test suites

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,16 +1,22 @@
-import { Component, ViewChild, ElementRef, Input } from '@angular/core';
+import { Component, ViewChild, ElementRef, Input, OnInit } from '@angular/core';
 import * as bootstrap from 'bootstrap';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html'
 })
 export class ModalComponent {
+  message: string = "SUCCESS";
 
-  @Input('message') message!: string;
-
-  openModal() {
-    const bsModal = new bootstrap.Modal(document.getElementById('modal') as HTMLElement);
+  openModal(msg: string = "SUCCESS") {
+    this.message = msg;
+    const bsModal = new Modal(document.getElementById('modal') as HTMLElement);
     bsModal.show();
+  }
+
+  hideModal() {
+    const bsModal = new Modal(document.getElementById('modal') as HTMLElement);
+    bsModal.hide();
   }
 }
--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,32 +1,73 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ModalComponent } from '../modal/modal.component';
 
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
+  let registerComponent: RegisterComponent;
+  let modalComponent: ModalComponent;
+
+  let fixtureRegister: ComponentFixture<RegisterComponent>;
+  let fixtureModal: ComponentFixture<ModalComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         RegisterComponent,
         ModalComponent
-      ]
+      ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule
+      ],
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixtureRegister = TestBed.createComponent(RegisterComponent);
+    registerComponent = fixtureRegister.componentInstance;
+    fixtureRegister.detectChanges();
 
-    component.fg.reset()
+    fixtureModal = TestBed.createComponent(ModalComponent);
+    modalComponent = fixtureModal.componentInstance;
+    fixtureModal.detectChanges();
+  });
+
+  afterEach(() =>{
+    registerComponent.fg.reset()
+    modalComponent.hideModal();
   });
 
   test('should create', () => {
-    expect(component).toBeTruthy();
+    expect(registerComponent).toBeTruthy();
   });
 
+  test('submit success', () => {
+
+    // arrange
+    const dataTest = {
+      name: "test",
+      email: "test@mail.com",
+      password: "abc12345",
+      confirmPassword: "abc12345"
+    }
+    registerComponent.fg.patchValue(dataTest);
+
+    // act
+    registerComponent.onSubmit();
+    fixtureRegister.detectChanges();
+    fixtureModal.detectChanges();
+
+    // assert
+    expect(registerComponent.fg.valid).toBeTruthy();
+
+    const input = fixtureRegister.nativeElement.querySelector('input') as HTMLElement;
+    expect(input.className).not.toMatch('is-invalid');
+
+    const modal = fixtureModal.nativeElement.querySelector('#modal') as HTMLElement;
+    expect(modal.className).toEqual('modal show');
+
+  })
 
   // validate fields when click submit
   // 1. field name should required
@@ -43,15 +84,15 @@ describe('RegisterComponent', () => {
   test('field name should required and show message', () => {
 
     // act
-    component.onSubmit();
-    fixture.detectChanges();
+    registerComponent.onSubmit();
+    fixtureRegister.detectChanges();
 
     // assert
-    expect(component.fg.get('name')?.invalid).toBeTruthy();
-    expect(component.fg.get('name')?.hasError('required')).toBeTruthy();
+    expect(registerComponent.fg.get('name')?.invalid).toBeTruthy();
+    expect(registerComponent.fg.get('name')?.hasError('required')).toBeTruthy();
 
-    const input = fixture.nativeElement.querySelector('#name') as HTMLElement;
-    const msg = fixture.nativeElement.querySelector('#name-required') as HTMLElement;
+    const input = fixtureRegister.nativeElement.querySelector('#name') as HTMLElement;
+    const msg = fixtureRegister.nativeElement.querySelector('#name-required') as HTMLElement;
     expect(msg.textContent).toEqual('Please enter your name.');
     expect(input.className).toMatch('is-invalid');
   });
@@ -59,15 +100,15 @@ describe('RegisterComponent', () => {
   test('field email should required and show message', () => {
 
     // act
-    component.onSubmit();
-    fixture.detectChanges();
+    registerComponent.onSubmit();
+    fixtureRegister.detectChanges();
 
     // assert
-    expect(component.fg.get('email')?.invalid).toBeTruthy();
-    expect(component.fg.get('email')?.hasError('required')).toBeTruthy();
+    expect(registerComponent.fg.get('email')?.invalid).toBeTruthy();
+    expect(registerComponent.fg.get('email')?.hasError('required')).toBeTruthy();
 
-    const input = fixture.nativeElement.querySelector('#email') as HTMLElement;
-    const msg = fixture.nativeElement.querySelector('#email-required') as HTMLElement;
+    const input = fixtureRegister.nativeElement.querySelector('#email') as HTMLElement;
+    const msg = fixtureRegister.nativeElement.querySelector('#email-required') as HTMLElement;
     expect(msg.textContent).toEqual('Please enter your email address.');
     expect(input.className).toMatch('is-invalid');
   });
@@ -75,46 +116,20 @@ describe('RegisterComponent', () => {
   test('field email should validate email pattern and show message', () => {
 
     // arrange
-    component.fg.get('email')?.setValue('invalid-email-pattern');
+    registerComponent.fg.get('email')?.setValue('invalid-email-pattern');
 
     // act
-    component.onSubmit();
-    fixture.detectChanges();
+    registerComponent.onSubmit();
+    fixtureRegister.detectChanges();
 
     // assert
-    expect(component.fg.get('email')?.invalid).toBeTruthy();
-    expect(component.fg.get('email')?.hasError('email')).toBeTruthy();
+    expect(registerComponent.fg.get('email')?.invalid).toBeTruthy();
+    expect(registerComponent.fg.get('email')?.hasError('email')).toBeTruthy();
 
-    const input = fixture.nativeElement.querySelector('#email') as HTMLElement;
-    const msg = fixture.nativeElement.querySelector('#email-pattern') as HTMLElement;
+    const input = fixtureRegister.nativeElement.querySelector('#email') as HTMLElement;
+    const msg = fixtureRegister.nativeElement.querySelector('#email-pattern') as HTMLElement;
     expect(msg.textContent).toEqual('Please enter a valid email address pattern.');
     expect(input.className).toMatch('is-invalid');
   });
 
-
-  test('submit success', () => {
-
-    // arrange
-    const dataTest = {
-      name: "test",
-      email: "test@mail.com",
-      password: "abc12345",
-      confirmPassword: "abc12345"
-    }
-    component.fg.patchValue(dataTest);
-
-    // act
-    component.onSubmit();
-    fixture.detectChanges();
-
-    // assert
-    expect(component.fg.valid).toBeTruthy();
-
-    const input = fixture.nativeElement.querySelector('input') as HTMLElement;
-    expect(input.className).not.toMatch('is-invalid');
-
-    const modal = fixture.nativeElement.querySelector('#modal') as HTMLElement;
-    expect(modal.className).toEqual('modal show');
-
-  })
 });
